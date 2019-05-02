069de2a30d612bf287ea3d1c75c8077d8550512a
Gregorio Lozada <greglozada@gmail.com>
2019-05-02 16:05:28 -0400

    Updates

---
+++ precache-manifest.b339bd0688845ff9faa42bad89b15d21.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "dcae97d9859f072db38d6dd3562aba89",
+    "url": "/RoboFighterPage/static/media/RoboFighterHeader.dcae97d9.png"
+  },
+  {
+    "revision": "af5348721ea26059d2ae3f43a25a4261",
+    "url": "/RoboFighterPage/static/media/ControlsPanel.af534872.png"
+  },
+  {
+    "revision": "190c84547ea9bda9ecf03f9a7ecd173a",
+    "url": "/RoboFighterPage/static/media/CanvasPanel.190c8454.png"
+  },
+  {
+    "revision": "531f0e2120da73ccff3f",
+    "url": "/RoboFighterPage/static/js/runtime~main.531f0e21.js"
+  },
+  {
+    "revision": "1eab707c679762a7011e",
+    "url": "/RoboFighterPage/static/js/main.1eab707c.chunk.js"
+  },
+  {
+    "revision": "eaf19d287cb7c04d2518",
+    "url": "/RoboFighterPage/static/js/2.eaf19d28.chunk.js"
+  },
+  {
+    "revision": "1eab707c679762a7011e",
+    "url": "/RoboFighterPage/static/css/main.c1acad09.chunk.css"
+  },
+  {
+    "revision": "6618392ee83c86c08017b9816552418b",
+    "url": "/RoboFighterPage/index.html"
+  }
+];
